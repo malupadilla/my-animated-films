05db4fd281c831117b165c530ea49f8a6b788026
malupadilla <145455169+malupadilla@users.noreply.github.com>
2024-09-16 18:37:54 -0300

    Atualizar o script.js
    aaaaa

--- Src/javascript/script.js
+++ Src/javascript/script.js
@@ -1,18 +1,18 @@
 let count = 0;
 
-// Verifica se existe um valor armazenado para 'count' no Local Storage
+// Verifica se existe um valor armazenado para 'count4' no Local Storage
 if (localStorage.getItem('count4')) {
     count = parseInt(localStorage.getItem('count4'));
 }
 
-// Atualiza o elemento HTML com o valor de 'count' ao carregar a página
+// Atualiza o elemento HTML com o valor de 'count4' ao carregar a página
 const value = document.getElementById('value');
 const plusButton = document.getElementById('plus');
 const minusButton = document.getElementById('minus');
 const resetButton = document.getElementById('reset');
 
 const updateValue = () => {
-    value.innerHTML = count;
+    value.innerHTML = count4;
 };
 
 updateValue(); /* Clique Normal */
@@ -20,17 +20,17 @@ updateValue(); /* Clique Normal */
 plusButton.addEventListener('click', () => {
     count += 1;
     updateValue();
-    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+    localStorage.setItem('count4', count); // Armazena o valor atualizado no Local Storage
 });
 
 minusButton.addEventListener('click', () => {
     count -= 1;
     updateValue();
-    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+    localStorage.setItem('count4', count); // Armazena o valor atualizado no Local Storage
 });
 
 resetButton.addEventListener('click', () => {
     count = 0;
     updateValue();
-    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
+    localStorage.setItem('count4', count); // Armazena o valor atualizado no Local Storage
 });
